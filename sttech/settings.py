34d1d4ba952836f3744885edbc2a7277545b744f
shoukreytom <shoukreytom01@gmail.com>
2020-08-11 20:45:26 +0200

    updated DEBUG value

--- sttech/settings.py
+++ sttech/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_VALUE") == "True"
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['sttech.herokuapp.com', 'localhost']
 
