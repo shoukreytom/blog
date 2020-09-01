6f20283c09b811f86f38cad70d2d9049fa686067
shoukreytom <shoukreytom01@gmail.com>
2020-09-01 19:37:40 +0200

    added summary field

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-	title = models.CharField(max_length=50, blank=False, null=False)
-	body = models.TextField()
-	date_posted = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=200)
+    body = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
