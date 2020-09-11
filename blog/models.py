624920c605046de47f42d88e64183657fbe0011d
shoukreytom <shoukreytom01@gmail.com>
2020-09-12 00:08:16 +0200

    updated summary field from char to text

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=200)
     body = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
 
