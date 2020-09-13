3b48efd8bdaaa11a561bb11042a51567c348b2da
shoukreytom <shoukreytom01@gmail.com>
2020-09-13 21:54:37 +0200

    added placeholders to the fields

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,9 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    message = forms.CharField(max_length=150, widget=forms.Textarea)
+    name = forms.CharField(max_length=30, widget=forms.TextInput(
+        attrs={'placeholder': 'full name'}))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'placeholder': 'joe@example.com'}))
+    message = forms.CharField(max_length=150, widget=forms.Textarea(
+        attrs={'placeholder': 'leave a message here'}))
