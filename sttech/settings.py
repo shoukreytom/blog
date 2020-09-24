fc1ac5f61d1246d94eff4b0540f96ccbfad8bf3f
shoukreytom <shoukreytom01@gmail.com>
2020-09-24 06:08:39 +0200

    secured email user and email password

--- sttech/settings.py
+++ sttech/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shoukreytom01@gmail.com'
-EMAIL_HOST_PASSWORD = 'xjdcnywhoqkiecfs'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
