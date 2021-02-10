da2c5638fb352ba3f41733f86b898424b2e6f14c
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-02-10 23:57:40 +0200

    fix KeyError bug at Admin (add Post)

--- blog/admin.py
+++ blog/admin.py
@@ -10,9 +10,6 @@ class PostAdmin(SummernoteModelAdmin, admin.ModelAdmin):
     list_display = ('title', 'content', 'status')
     list_filter = ('publish', 'update', 'status')
     search_fields = ('title', 'content')
-    prepopulated_fields = {
-        'slug': ('title', )
-    }
 
 
 admin.site.register(Post, PostAdmin)
