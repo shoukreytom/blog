e20f544521683a6f7639fe092a8a8a52031e6200
Shoukrey Tom <abdulshakoor.abdulazeem@gmail.com>
2021-03-20 14:31:42 +0200

    configure email vars for heroku

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,12 @@ else:
     default_key = secrets.token_hex(24)
     SECRET_KEY = os.environ.get("SECRET_KEY", default_key)
     DEBUG = os.environ.get("DEBUG_VALUE", False)
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    EMAIL_PORT = 25
 
 ALLOWED_HOSTS = ["*"]
 
